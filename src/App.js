5dab6269982ec06698cd9214a79ea20467e0abaa
Carson Brown <carsonbrown47@gmail.com>
2023-04-12 16:32:35 -0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import React, { useState } from 'react'
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React from 'react';
+
 
 function App() {
-	const DUMMY_EXPENSES = [
-		{
-			title: "Toilet Paper",
-			amount: 94.12,
-			date: new Date(2020, 7, 14),
-		},
-		{
-			title: "New TV",
-			amount: 799.49,
-			date: new Date(2021, 2, 12),
-		},
-		{
-			title: "Car Insurance",
-			amount: 294.67,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			title: "New Desk (Wooden)",
-			amount: 450,
-			date: new Date(2021, 5, 12),
-		},
-	];
+  return (
+    <div>
 
-	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-	const addExpenseHandler = (expense) => {
-		setExpenses(prevExpenses => {
-			return [expense, ...prevExpenses];
-		});
-	};
-
-	return (
-		<div>
-			<NewExpense onAddExpense={addExpenseHandler} />
-			<Expenses items={expenses} />
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
