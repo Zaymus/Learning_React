8a9b8380b19f5dcf531aa96741c8fae111bcd394
Carson Brown <carsonbrown47@gmail.com>
2023-04-12 19:49:03 -0400

    Completed section 8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const newUserHandler = (user) => {
+    setUsersList((prevState) => {
+      return [
+        ...prevState,
+        user
+      ]
+    });
+  }
+
   return (
     <div>
-
+      <AddUser onNewUser={newUserHandler}/>
+      <UserList users={usersList} />
     </div>
   );
 }
